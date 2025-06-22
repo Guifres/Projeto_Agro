a6a5f27dc276ac15596067c1f1231cf15d17814b
Guilherme Freitas <gui2010mg68@gmail.com>
2025-06-22 09:54:42 -0300

    .

--- javascript/scritp.js
+++ javascript/scritp.js
@@ -1,2 +1,16 @@
-let milho = document.querySelector('#milho')
-console.log(milho)
+function calcular(){
+    let milho = document.getElementById("milho").value
+    let soja = document.getElementById("soja").value
+    let nucleo = document.getElementById("nucleo").value
+    milho = milho * 70 / 100
+    soja = soja * 27 / 100
+    nucleo = nucleo * 3 / 100
+    var proteina = Number(milho) + Number(soja) + Number(nucleo)
+    return res.innerHTML =`<p>Resultado milho ${proteina} ${milho} ${soja} ${nucleo}<p/>`
+
+}
+
+
+
+
+
