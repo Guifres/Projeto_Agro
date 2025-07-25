8c5a41ed50d300c36312b390ad46bbfa3ea24fb7
Guilherme Freitas <gui2010mg68@gmail.com>
2025-07-25 16:34:17 -0300

    Atualização

--- javascript/scritp.js
+++ javascript/scritp.js
@@ -1,12 +1,22 @@
 function calcular(){
-    let milho = document.getElementById("milho").value
-    let soja = document.getElementById("soja").value
-    let nucleo = document.getElementById("nucleo").value
-    milho = milho * 70 / 100
-    soja = soja * 27 / 100
-    nucleo = nucleo * 3 / 100
-    var proteina = Number(milho) + Number(soja) + Number(nucleo)
-    return res.innerHTML =`<p>Resultado milho ${proteina} ${milho} ${soja} ${nucleo}<p/>`
+    let PesoLeite = document.getElementById("PesoLeite").value
+    let QuantidadeRacao = document.getElementById("QuantidadeRacao").value
+    let PrecoLeite = document.getElementById("PrecoLeite").value
+    let PrecoRacao = document.getElementById('PrecoRacao').value
+    let ValorEmReaisLeite = PesoLeite * PrecoLeite
+    let ValorEmReaisRacao = QuantidadeRacao * PrecoRacao
+    let LucroDia = ValorEmReaisLeite - ValorEmReaisRacao
+    let LucroMes = LucroDia * 30
+    let PorcentagemLucro = LucroDia * 100 / ValorEmReaisLeite
+    let PorcetagemGasto = ValorEmReaisRacao * 100 / ValorEmReaisLeite
+    return res.innerHTML =`<p>Leite bruto em Reais/Mês: R$${ValorEmReaisLeite.toFixed(2)*30}<br>
+    Produção leite em Reais: R$${PesoLeite*PrecoLeite}<br>
+    Custo ração dia: R$${QuantidadeRacao*PrecoRacao}<br>
+    Lucro Dia: R$${LucroDia.toFixed(2)}<br>
+    Lucro Mês: R$${LucroMes.toFixed(2)}<br>
+    Porcentagem de Lucro: ${PorcentagemLucro.toFixed(2)} % <br>
+    Porcentagem de Gasto esta de: R$${PorcetagemGasto.toFixed(2)} % <br> 
+    <p/>`
 
 }
 
